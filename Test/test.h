aaa82a8c59133e395d12078ca8defb358248e613
liweijie_ubuntu <helloworld.liweili@gmail.com>
2022-12-22 22:07:10 +0800

    :tada: Version 0.0, basic test framework complete!

--- Test/test.h
+++ Test/test.h
@@ -506,8 +506,173 @@ EXPECT_CON_EQ(v1, v3)   ok
     std::cout << " " << str_name << " : " << str << "\n";   \
 } while(0)
 
+// 输出容器调用函数后的结果
+#define FUN_AFTER(con, fun) do {                            \
+    std::string fun_name = #fun;                            \
+    std:cout << " After " << fun_name << " :\n";            \
+    fun;                                                    \
+    COUT(con);                                              \
+} while(0)
+
+#define STR_FUN_AFTER(str, fun) do {                        \
+    std::string fun_name = #fun;                            \
+    std:cout << " After " << fun_name << " :\n";            \
+    fun;                                                    \
+    STR_COUT(str);                                          \
+} while(0)
+
+// 输出容器调用函数的值
+#define FUN_VALUE(fun) do {                                 \
+    std::string fun_name = #fun;                            \
+    std::cout << " " << fun_name << " : " << fun << "\n";   \
+} while(0)
+
+// 输出测试数量级
+void test_len(size_t len1, size_t len2, size_t len3, size_t wide) {
+    std::string str1, str2, str3;
+    std::stringstream ss;
+    ss << len1 << " " << len2 << " " << len3;
+    ss >> str1 >> str2 >> str3;
+    str1 += "   |";
+    std::cout << std::setw(wide) << str1;
+    str2 += "   |";
+    std::cout << std::setw(wide) << str2;
+    str3 += "   |";
+    std::cout << std::setw(wide) << str3 << "\n";
+}
+
+#define TEST_LEN(len1, len2, len3, wide) \
+    test_len(len1, len2, len3, wide)
+
+// 常用测试性能的宏
+#define FUN_TEST_FORMAT1(mode, fun, arg, count) do {            \
+    srand((int)time(0));                                        \
+    clock_t start, end;                                         \
+    mode c;                                                     \
+    char buf[10];                                               \
+    start = clock();                                            \
+    for (size_t i = 0; i < count; i++) c.fun(arg);              \
+    end = clock();                                              \
+    int n = static_cast<int>(static_cast<double>(end - start)   \
+            / CLOCKS_PER_SEC * 1000);                           \
+    std::snprintf(buf, sizeof(buf), "%d", n);                   \
+    std::string t = buf;                                        \
+    t += "ms    |";                                             \
+    std::cout << std::setw(WIDE) << t;                          \
+} while(0)
+
+#define FUN_TEST_FORMAT2(mode, fun, arg1, arg2, count) do {     \
+    srand((int)time(0));                                        \
+    clock_t start, end;                                         \
+    mode c;                                                     \
+    char buf[10];                                               \
+    start = clock();                                            \
+    for (size_t i = 0; i < count; ++i) c.fun(c.arg1(), arg2);   \
+    end = clock();                                              \
+    int n = static_cast<int>(static_cast<double>(end - start)   \
+            / CLOCKS_PER_SEC * 1000);                           \
+    std::snprintf(buf, sizeof(buf), "%d", n);                   \
+    std::string t = buf;                                        \
+    t += "ms    |";                                             \
+    std::cout << std::setw(WIDE) << t;                          \
+} while(0)
+
+#define LIST_SORT_DO_TEST(mode, count) do {                     \
+    srand((int)time(0));                                        \
+    clock_t start, end;                                         \
+    mode::list<int> l;                                          \
+    char buf[10];                                               \
+    for (size_t i = 0; i < count; ++i)                          \
+        l.insert(l.end(), rand());                              \
+    start = clock();                                            \
+    l.sort();                                                   \
+    end = clock();                                              \
+    int n = static_cast<int>(static_cast<double>(end - start)   \
+            / CLOCKS_PER_SEC * 1000);                           \
+    std::snprintf(buf, sizeof(buf), "%d", n);                   \
+    std::string t = buf;                                        \
+    t += "ms    |";                                             \
+    std::cout << std::setw(WIDE) << t;                          \
+} while(0)
+
+#define MAP_EMPLACE_DO_TEST(mode, con, count) do {              \
+    srand((int)time(0));                                        \
+    clock_t start, end;                                         \
+    mode::con<int, int> c;                                      \
+    char buf[10];                                               \
+    start = clock();                                            \
+    for (size_t i = 0; i < count; ++i)                          \
+        c.emplace(mode::make_pair(rand(), rand()));             \
+    end = clock();                                              \
+    int n = static_cast<int>(static_cast<double>(end - start)   \
+            / CLOCKS_PER_SEC * 1000);                           \
+    std::snprintf(buf, sizeof(buf), "%d", n);                   \
+    std::string t = buf;                                        \
+    t += "ms    |";                                             \
+    std::cout << std::setw(WIDE) << t;                          \
+} while(0)
+
+// 重构重复代码
+#define CON_TEST_P1(con, fun, arg, len1, len2, len3)            \
+    TEST_LEN(len1, len2, len3, WIDE);                           \
+    std::cout << "|         std         |";                     \
+    FUN_TEST_FORMAT1(std::con, fun, arg, len1);                 \
+    FUN_TEST_FORMAT1(std::con, fun, arg, len2);                 \
+    FUN_TEST_FORMAT1(std::con, fun, arg, len3);                 \
+    std::cout << "\n|        mystl        |";                   \
+    FUN_TEST_FORMAT1(mystl::con, fun, arg, len1);               \
+    FUN_TEST_FORMAT1(mystl::con, fun, arg, len2);               \
+    FUN_TEST_FORMAT1(mystl::con, fun, arg, len3); 
+
+#define CON_TEST_P2(con, fun, arg1, arg2, len1, len2, len3)     \
+    TEST_LEN(len1, len2, len3, WIDE);                           \
+    std::cout << "|         std         |";                     \
+    FUN_TEST_FORMAT2(std::con, fun, arg1, arg2, len1);          \
+    FUN_TEST_FORMAT2(std::con, fun, arg1, arg2, len2);          \
+    FUN_TEST_FORMAT2(std::con, fun, arg1, arg2, len3);          \
+    std::cout << "\n|        mystl        |";                   \
+    FUN_TEST_FORMAT2(mystl::con, fun, arg1, arg2, len1);        \
+    FUN_TEST_FORMAT2(mystl::con, fun, arg1, arg2, len2);        \
+    FUN_TEST_FORMAT2(mystl::con, fun, arg1, arg2, len3);   
+
+#define MAP_EMPLACE_TEST(con, len1, len2, len3)                 \
+    TEST_LEN(len1, len2, len3, WIDE);                           \
+    std::cout << "|         std         |";                     \
+    MAP_EMPLACE_DO_TEST(std, con, len1);                        \
+    MAP_EMPLACE_DO_TEST(std, con, len2);                        \
+    MAP_EMPLACE_DO_TEST(std, con, len3);                        \
+    std::cout << "\n|        mystl        |";                   \
+    MAP_EMPLACE_DO_TEST(mystl, con, len1);                      \
+    MAP_EMPLACE_DO_TEST(mystl, con, len2);                      \
+    MAP_EMPLACE_DO_TEST(mystl, con, len3);
+
+#define LIST_SORT_TEST(len1, len2, len3)                        \
+    TEST_LEN(len1, len2, len3, WIDE);                           \
+    std::cout << "|         std         |";                     \
+    LIST_SORT_DO_TEST(std, len1);                               \
+    LIST_SORT_DO_TEST(std, len2);                               \
+    LIST_SORT_DO_TEST(std, len3);                               \
+    std::cout << "\n|        mystl        |";                   \
+    LIST_SORT_DO_TEST(mystl, len1);                             \
+    LIST_SORT_DO_TEST(mystl, len2);                             \
+    LIST_SORT_DO_TEST(mystl, len3);
+
+// 简单测试的宏定义
+#define TEST(testcase_name) MYTINYSTL_TEST_(testcase_name)
+
+// 运行所有测试案例
+#define RUN_ALL_TESTS() mystl::test::UnitTest::GetInstance()->Run()
+
+// 是否开启性能测试
+#ifndef RERFORMANCE_TEST_ON
+#define PERFORMANCE_TEST_ON 1
+#endif // !PERFORMANCE_TEST_ON
+
+// 是否开启大数据量测试
+#ifndef LARGER_TEST_DATA_ON
+#define LARGER_TEST_DATA_ON 0
+#endif // !LARGER_TEST_DATA_ON
+
 }// namespace test
 }// namespace mystl
-
-
 #endif //! MYTINYSTL_TEST_H_
